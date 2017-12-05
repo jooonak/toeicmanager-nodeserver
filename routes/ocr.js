6e401d58d34eb06e7efd9f1c2c1f11e244bf6c29
hb <nan_hongbin@naver.com>
2017-12-05 09:52:30 +0900

    1. commit

--- routes/ocr.js
+++ routes/ocr.js
@@ -19,7 +19,12 @@ router.post('/', function(req, res, next) {
     //console.log(req.fields); // contains non-file fields
     //console.log(req.files); // contains files
 
+    //console.log(req.fields.file);
+    //console.log(req.files.file.path);
     const fileName = req.files.file.path;
+    //console.log("1111111111"+fileName);
+
+    //const fileName = "data:image/jpeg;base64,"+req.fields.file.path;
 
     //Error: Can't set headers after they are sent.에 대한 내용은 아래 url을 따라 가보자
     //https://stackoverflow.com/questions/7042340/error-cant-set-headers-after-they-are-sent-to-the-client 
@@ -27,10 +32,10 @@ router.post('/', function(req, res, next) {
     vision.documentTextDetection({ source: { filename: fileName } })
         .then((results) => {
         const fullTextAnnotation = results[0].fullTextAnnotation;
-    //console.log(fullTextAnnotation.text);
-    //var text =fullTextAnnotation.text;
-    //res.json(text);
-    res.json(fullTextAnnotation.text);
+    console.log(fullTextAnnotation.text);
+    var text =fullTextAnnotation.text;
+    res.json(text);
+    //res.json(fullTextAnnotation.text);
     }).catch((err) => {
             console.error('ERROR:', err);
     });
