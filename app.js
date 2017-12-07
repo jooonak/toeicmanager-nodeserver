9aff635106b3bf8996c36f04e01b1017b0102cc4
jooonak <dlwnsgkr11_@naver.com>
2017-12-07 10:23:55 +0900

    1. FCM server 구현, 추후 스케줄링 관련 추가구현 여부 결정

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var users = require('./routes/users');
 var dic = require('./routes/dic');
 var ocr = require('./routes/ocr');
 var result = require('./routes/result');
+var fcm = require('./routes/fcm');
 
 var app = express();
 
@@ -34,6 +35,7 @@ app.use('/users', users);
 app.use('/dic', dic);
 app.use('/ocr', ocr);
 app.use('/result', result);
+app.use('/fcm', fcm);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
