0668635cd9b06667661ef5aeba2b98049001aac5
jooonak <dlwnsgkr11_@naver.com>
2017-12-08 17:21:39 +0900

    1. 문장 크롤링 사이트 추가 -> 둘 중 하나 선택해서 사용

--- routes/crawling.js
+++ routes/crawling.js
@@ -6,10 +6,10 @@ var request = require('request');
 /* GET home page. */
 router.get('/sentences', function(req, res, next) {
 
-    var word = 'kind'//req.query.word;
+    var word = req.query.word;
 
     function getSentences(word, callback) {
-        var url = 'https://en.oxforddictionaries.com/definition/' + word; //word
+        var url = 'https://en.oxforddictionaries.com/definition/' + word;
         request(url, function (err, response, html) {
             if (err) {console.log(err)};
 
@@ -27,9 +27,38 @@ router.get('/sentences', function(req, res, next) {
     })
 });
 
+// 문장 크롤링 가능한 사이트2 위와 아래 중 선택해서 사용 가능
+// router.get('/sentences', function(req, res, next) {
+//
+//     var word = req.query.word;
+//
+//     function getSentences(word, callback) {
+//         var url = 'http://sentence.yourdictionary.com/' + word;
+//         request(url, function (err, response, html) {
+//             if (err) {console.log(err)};
+//
+//             var $ = cheerio.load(html);
+//             var sentences = [];
+//
+//             $('.li_content').each(function (index, item) {
+//                 var sentence = $(this).text();
+//
+//                 console.log(sentence);
+//                 sentences.push(sentence);
+//
+//                 if (index === 3) return false;
+//             });
+//             return callback(sentences);
+//         });
+//     }
+//     getSentences(word, function (result) {
+//         res.json({sentences : result});
+//     })
+// });
+
 router.get('/img', function(req, res, next) {
 
-    var word = 'kind'//req.query.word;
+    var word = req.query.word;
 
     function getImgsSrc(word, callback) {
         var url = 'https://www.google.co.kr/search?q=' + word + '&source=lnms&tbm=isch&sa=X&ved=0ahUKEwj33Y7qofnXAhVCl5QKHUimBFEQ_AUICygC&biw=1121&bih=949';
